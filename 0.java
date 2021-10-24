9cb5dfc38bd73088828f313b87c73ee7206af562
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-10-24 12:55:03 +0300

    before

---
+++ 0.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Comment
+        int x;
+        x = 5;
+        while(x % 2 != 0)
+            x += 1;
+        x += 1;
+    }
+}
