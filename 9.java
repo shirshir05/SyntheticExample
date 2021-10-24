ac886ef53d01f0dfa8eeeb927acde7efbaaed62b
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-10-24 12:55:08 +0300

    before

---
+++ 9.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x;
+        x = 5;
+        if (x % 2 == 0)
+            x = x + 1;
+        else
+            x += 2;
+    }
+}
