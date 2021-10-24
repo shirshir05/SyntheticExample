75d9f83fc5f727ebb993a9dc990841c2adc6cad0
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-10-24 12:55:04 +0300

    before

---
+++ 1.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x;
+        x = 5;
+        while(x % 2 != 0)
+            x += 1;
+        x += 1;
+    }
+}
