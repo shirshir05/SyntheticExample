171e0ec6e1b10ac3190b931ded2c289bb744c5a1
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-10-24 12:55:06 +0300

    before

---
+++ 4.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x;
+        x = 5;
+        for(int i = 0;x % 2 != 0;i++)
+            x += 1;
+        x += 1;
+    }
+}
